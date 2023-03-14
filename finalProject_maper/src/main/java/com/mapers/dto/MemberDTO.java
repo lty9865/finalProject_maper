e4ca52b4c37a64e40a9b2de6c0d7b0836ab189a4
lty9865 <117057955+lty9865@users.noreply.github.com>
2023-03-14 16:38:33 +0900

    dto 수정

--- finalProject_maper/src/main/java/com/mapers/dto/MemberDTO.java
+++ finalProject_maper/src/main/java/com/mapers/dto/MemberDTO.java
@@ -1,18 +1,25 @@
 package com.mapers.dto;
 
 public class MemberDTO {
-	
+
 	private String userid;
 	private String password;
 	private String email;
 	private String birth;
 	private int admins;
-	
-	/* licencekey 추후 회의 후 추가
-	private String lisencekey; */
-	
-	/* blob 은 추후 회의 진행 후 추가
-	private blob */
+	private String licensekey;
+
+	/*
+	 * blob 은 추후 회의 진행 후 추가 private blob
+	 */
+
+	public String getLicensekey() {
+		return licensekey;
+	}
+
+	public void setLicensekey(String licensekey) {
+		this.licensekey = licensekey;
+	}
 
 	public String getUserid() {
 		return userid;
@@ -53,5 +60,5 @@ public class MemberDTO {
 	public void setAdmins(int admins) {
 		this.admins = admins;
 	}
-	
+
 }
