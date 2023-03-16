835ce985a452c855b4f69142a0967a5fa5e1ae64
cycotest <aceldamadk@gamil.com>
2023-03-16 15:14:25 +0900

    MemberDAO 에서 DB Connection Pool 파트를 분리

--- finalProject_maper/src/main/java/com/mapers/dao/MemberDAO.java
+++ finalProject_maper/src/main/java/com/mapers/dao/MemberDAO.java
@@ -7,33 +7,21 @@ import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.sql.DataSource;
 
+import com.mapers.common.DBConnPool;
 import com.mapers.dto.MemberDTO;
 
-public class MemberDAO {
-	private Connection conn;
+public class MemberDAO extends DBConnPool {
 	private PreparedStatement psmt;
 	private Context initContext;
 	private Context envContext;
 
-	private MemberDAO() {
-	}
+	private MemberDAO() {}
 
 	// 싱글턴 패턴으로 인스턴스 생성 제한
 	private static MemberDAO instance = new MemberDAO();
 
 	public static MemberDAO getInstance() {
 		return instance;
-	}
-
-	// 커넥트 풀 생성
-	public Connection getConnection() throws Exception {
-		conn = null;
-		initContext = new InitialContext();
-		envContext = (Context) initContext.lookup("java:/comp/env");
-		DataSource ds = (DataSource) envContext.lookup("jdbc/myoracle");
-		conn = ds.getConnection();
-		
-		return conn;
 	}
 
 	// 백엔드 코드 작성 예시
@@ -64,4 +52,6 @@ public class MemberDAO {
 		
 		return result;
 	}
+	
+	
 }
